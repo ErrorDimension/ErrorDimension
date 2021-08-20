b5750ea473f1f36dd44511e757244b6c5ffbe9c6
ErrorDIM <mrxiaolin2005@gmail.com>
2021-08-20 21:51:04 +0700

    d

--- Update.py
+++ Update.py
@@ -1,16 +1,36 @@
+from datetime import datetime
 import pytz
 import requests
 import json
 from time import perf_counter, time
-from lib import ehook
+import lib
 import logging
 import colorama
 from colorama import Fore
 colorama.init()
 
-from datetime import datetime
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger("OKAY", "Imported: colorama")
+logger("OKAY", "Imported: datetime.datetime")
+logger("OKAY", "Imported: pytz")
+logger("OKAY", "Imported: time.perf_counter")
+logger("OKAY", "Imported: requests")
+logger("OKAY", "Imported: json")
+
+def logStatus(text, status, overWrite=False):
+    statusText = [f"{Fore.RED}✗ ERRR",
+                  f"{Fore.YELLOW}● WAIT", f"{Fore.GREEN}✓ OKAY"]
+    logStatus = ["ERRR", "INFO", "OKAY"]
+    logger(logStatus[status + 1], "{:48}{}{}".format(text,
+                                                  statusText[status + 1], Fore.RESET), resetCursor=(not overWrite))
+
+
+USER_DATA = None
+
 try:
-	USER_DATA = requests.get("https://api.github.com/users/10errordim").json()
+    USER_DATA = requests.get("https://api.github.com/users/10errordim").json()
 except json.JSONDecodeError as error:
-	
-	raise error
+    logStatus("Parse User Data Failed: Malformed JSON Data", -1, True)
+    raise error
