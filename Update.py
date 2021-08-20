5b9d071ebdcc53c3977477ea93e981063ff7fc6a
ErrorDIM <mrxiaolin2005@gmail.com>
2021-08-20 21:30:57 +0700

    d

--- Update.py
+++ Update.py
@@ -4,14 +4,13 @@ import json
 from time import perf_counter, time
 from lib import ehook
 import logging
-
 import colorama
 from colorama import Fore
 colorama.init()
 
 from datetime import datetime
 try:
-	USER_DATA = requests.get("https://api.github.com/users/belivipro9x99").json()
+	USER_DATA = requests.get("https://api.github.com/users/10errordim").json()
 except json.JSONDecodeError as error:
-	logStatus("Parse User Data Failed: Malformed JSON Data", -1, True)
+	
 	raise error
